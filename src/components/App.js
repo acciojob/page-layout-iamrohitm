56f295befeecbbe38b913e1cca16476ba470a72f
Rohit Mohite <rohitmohite.in@gmail.com>
2026-05-07 15:27:10 +0530

    6th

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-
 import React from "react";
 import './../styles/App.css';
 import PageLayout from "./PageLayout";
 
 const App = () => {
-    let head = <h1>Welcome to my website</h1>
-    let foot = <div>© 2023 My Website. All rights reserved</div>
 
-    return (
-      <div>
-          {/* Do not remove the main div */}
-          <PageLayout header={head} footer={foot}>This is the content of my Website</PageLayout>
-      </div>
-    )
+  let header = <h1>Welcome to my website</h1>
+  let footer = <div>© 2023 My Website. All rights reserved.</div>
+  return (
+    <div>
+        {/* Do not remove the main div */}
+        <PageLayout header={header} footer={footer}>
+         <p>This is the content of my website.</p>
+        </PageLayout>
+    </div>
+  )
 }
 
 export default App
